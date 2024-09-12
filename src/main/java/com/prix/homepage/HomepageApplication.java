cf879482a9bf334ed5f0ad6c81fe05aaaf933264
NoDucksPlease <ryuhaksrg@gmail.com>
2024-09-12 22:29:03 +0900

    gradle war file build support
    gradle을 통해 war 파일로 컴파일 할 수 있도록 필요한 부분을 주석으로 추가

--- src/main/java/com/prix/homepage/HomepageApplication.java
+++ src/main/java/com/prix/homepage/HomepageApplication.java
@@ -2,9 +2,19 @@ package com.prix.homepage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/* import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; */
 
 @SpringBootApplication
-public class HomepageApplication {
+public class HomepageApplication{
+
+/* @SpringBootApplication
+public class HomepageApplication extends SpringBootServletInitializer{
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HomepageApplication.class);
+    } */
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomepageApplication.class, args);
